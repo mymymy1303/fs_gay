31cc59d56076dbc184fb94bd871e04e614c7e350
mymymy1303 <thehuy160897@gmail.com>
2018-08-30 11:04:01 +0700

    Add option of links btn class

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,2 +1,64 @@
-// Main
-console.log('FullStack Vietnam!')
+$(function () {
+	//THE DOM IS READY
+
+	// ------------------------------------
+	//-----------WELCOME SLIDER------------
+	//-------------------------------------
+	$('#welcome_slider').owlCarousel({
+		loop: true,
+		nav: false,
+		dots: false,
+		items: 1,
+		margin: 0
+	})
+
+
+	// // ---------------------------------
+	// //---------FEEDBACK SLIDER----------
+	// //----------------------------------
+	// $('#feedback_slider').owlCarousel({
+	// 	loop: false,
+	// 	items: 1,
+	// 	dots: true,
+	// 	dotsEach: 1,
+	// 	margin: 0
+	// })
+
+	// // -------------------------------------
+	// // -----------CLIENT-SLIDER-------------
+	// // -------------------------------------
+	// $("#client_slider").owlCarousel({
+	// 	loop: true,
+	// 	margin: 10,
+	// 	nav: true,
+	// 	dots: false,
+	// 	items: 1,
+	// 	smartSpeed: 500,
+	// 	responsive: {
+	// 		576: {
+	// 			items: 2
+	// 		},
+	// 		768: {
+	// 			//Adding autoplay slide for medium and above devices
+	// 			//Hover to pause autoplay
+	// 			autoplay: true,
+	// 			autoplayTimeout: 2000,
+	// 			autoplayHoverPause: true,
+	// 			items: 3,
+	// 			margin: 0
+	// 		},
+	// 		992: {
+	// 			autoplay: true,
+	// 			autoplayTimeout: 2000,
+	// 			autoplayHoverPause: true,
+	// 			items: 4
+	// 		},
+	// 		1200: {
+	// 			autoplay: true,
+	// 			autoplayTimeout: 2000,
+	// 			autoplayHoverPause: true,
+	// 			items: 6
+	// 		}
+	// 	}
+	// })
+});
